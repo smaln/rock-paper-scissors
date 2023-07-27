1c7a6a75714be169b8547a00c8cfa768552a06a5
Simon <simonmalnar.sm@gmail.com>
2023-07-27 10:43:06 +0200

    Completion of task

--- javascript-ext.js
+++ javascript-ext.js
@@ -1,8 +1,38 @@
+let compChoice;
+let playSelect=prompt('Enter "rock","paper" or "scissors"!');
+playSelect=playSelect.toLowerCase();
+
+let retInfo="abc";
+let op=0;
+let compScore=0, playScore=0;
+
 function getComputerChoice(){
-    let choice =("rock","paper","scissors");
-    
+  let arrChoice =["rock","paper","scissors"]
+  let randNum = Math.floor(Math.random()*3);
+  return compChoice=arrChoice[randNum];
 }
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+for(let i=0; i<5; i++){
+
+getComputerChoice(compChoice);
+console.log(compChoice);
+
+if(compChoice=="rock" && playSelect=="rock") {retInfo="Tie! Rock cannot beat Rock"; op=2;}
+else if(compChoice=="rock" && playSelect=="paper") {retInfo="You Win! Paper beats Rock"; op=1;}
+else if(compChoice=="rock" && playSelect=="scissors") {retInfo="You Lose! Rock beats Scissors"; op=0;}
+
+else if(compChoice=="paper" && playSelect=="rock") {retInfo="You Lose! Paper beats Rock"; op=0;}
+else if(compChoice=="paper" && playSelect=="paper") {retInfo="Tie! Paper cannot beat Paper"; op=2;}
+else if(compChoice=="paper" && playSelect=="scissors") {retInfo="You Win! Scissors beat Paper"; op=1;}
+
+else if(compChoice=="scissors" && playSelect=="rock") {retInfo="You Win! Rock beats Scissors"; op=1;}
+else if(compChoice=="scissors" && playSelect=="paper") {retInfo="You Lose! Scissors beat Paper"; op=0;}
+else if(compChoice=="scissors" && playSelect=="scissors") {retInfo="Tie! Scissors cannot beat Scissors"; op=2;}
+
+console.log(retInfo);
+
+if(op==0) {++compScore;}
+else if(op==1) {++playScore;}
 }
+
+console.log(compScore, playScore)
